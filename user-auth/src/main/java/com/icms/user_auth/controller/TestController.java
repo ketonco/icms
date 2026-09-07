8deb47be6715997ae04e0ba9aa44a0882db5ca72
Maximo Barroeta <ketonco@gmail.com>
2026-09-07 19:52:22 +0200

    user-auth service - test (add) Add TestController for user-auth service with a test endpoint

---
+++ user-auth/src/main/java/com/icms/user_auth/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.icms.user_auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class TestController {
+    
+    @GetMapping("/test")
+    public String test() {
+        return "User-Auth Service is running";
+    }
+
+}
