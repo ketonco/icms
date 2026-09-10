e062117d39c6c702974fca4e2d2c3469061cae33
Maximo Barroeta <ketonco@gmail.com>
2026-09-10 17:05:57 +0200

    feat(application) - Configure base packages for UserAuthApplication and add EntityScan

--- user-auth/src/main/java/com/icms/user_auth/UserAuthApplication.java
+++ user-auth/src/main/java/com/icms/user_auth/UserAuthApplication.java
@@ -2,8 +2,10 @@ package com.icms.user_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.icms.user_auth", "com.icms.shared"})
+@EntityScan(basePackages = {"com.icms.user_auth", "com.icms.shared"})
 public class UserAuthApplication {
 
 	public static void main(String[] args) {
