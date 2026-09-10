5aeefdd04901c549505fe5b18061867395017c7c
Maximo Barroeta <ketonco@gmail.com>
2026-09-10 16:41:33 +0200

    refactor(entity) - Replace languageId field with Many-to-One relationship to Language entity in BaseCatalogTranslationEntity

--- shared-kernel/src/main/java/com/icms/shared/entity/BaseCatalogTranslationEntity.java
+++ shared-kernel/src/main/java/com/icms/shared/entity/BaseCatalogTranslationEntity.java
@@ -28,8 +28,9 @@ public class BaseCatalogTranslationEntity<C> extends AuditableEntity {
     @JoinColumn(updatable = false, nullable = false, name="catalog_id")
     private C catalog; // Reference to the catalog entity this translation belongs to
 
-    @Column(updatable = false, nullable = false, name="language_id")
-    private Long languageId; // Reference to the language entity for this translation
+    @ManyToOne(fetch = FetchType.LAZY) // Many-to-one relationship with the language entity, FetchType.LAZY ensures the language is loaded only when accessed
+    @JoinColumn(updatable = false, nullable = false, name="language_id")
+    private Language language; // Reference to the language entity for this translation
 
     @Column(updatable = true, nullable = false, name="translation")
     private String translation; // The actual translation text for the catalog entity in the specified language
