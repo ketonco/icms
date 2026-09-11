08c07db76e7d8d140c96778560d9546d1e73ded4
Maximo Barroeta <ketonco@gmail.com>
2026-09-11 21:21:16 +0200

    feat(entity) - Add constructors to Language class for improved instantiation flexibility

--- shared-kernel/src/main/java/com/icms/shared/entity/Language.java
+++ shared-kernel/src/main/java/com/icms/shared/entity/Language.java
@@ -18,6 +18,18 @@ import lombok.Builder;
 @Builder // Lombok annotation to generate a builder pattern for this class, this means you can create instances of Language using a fluent API.
 public class Language extends BaseCatalogEntity {
 
+    public Language(String code, String name, Boolean isDefault, Boolean active) {
+        this.name = name;
+        this.isDefault = isDefault;
+        super.setActive(active);
+        super.setCode(code);
+    }
+
+    public Language(Long id, String code, String name, Boolean isDefault, Boolean active) {
+        this(code, name, isDefault, active);
+        super.setId(id);
+    }
+
     @Column(unique = true, updatable = true, nullable = false, name="name", length = 50)
     private String name; // Ej: English, Spanish, etc.
 
