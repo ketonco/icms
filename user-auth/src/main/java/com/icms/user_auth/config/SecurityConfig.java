019cd72c6f6e8ad55646d48688bea927a3812d07
Maximo Barroeta <ketonco@gmail.com>
2026-09-11 21:20:40 +0200

    feat(security) - Enhance SecurityConfig with detailed comments and maintain profile exclusion for task-related operations

--- user-auth/src/main/java/com/icms/user_auth/config/SecurityConfig.java
+++ user-auth/src/main/java/com/icms/user_auth/config/SecurityConfig.java
@@ -2,10 +2,21 @@ package com.icms.user_auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
+ * Security configuration for the application.
+ *
+ * This configuration ensures that security measures such as authentication and authorization are applied to the application endpoints.
+ * 
+ * This configuration is excluded when the 'task' profile is active.
+ * 
+ * this allows the application to bypass security configurations when running task-related operations. by example, scheduled tasks or background jobs can run without being affected by security constraints.
+ */
 @Configuration
+@Profile("!task") // Exclude this configuration when the 'task' profile is active because it is not needed for task-related operations
 public class SecurityConfig {
 
     @Bean
