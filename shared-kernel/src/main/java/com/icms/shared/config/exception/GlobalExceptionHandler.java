1ef9248e1badab67f1bf2a31d05c62d70dc8e7d2
Maximo Barroeta <ketonco@gmail.com>
2026-09-13 21:30:19 +0200

    refactor(GlobalExceptionHandler) - Update exception handling methods to return ResponseEntity for consistent response structure

--- shared-kernel/src/main/java/com/icms/shared/config/exception/GlobalExceptionHandler.java
+++ shared-kernel/src/main/java/com/icms/shared/config/exception/GlobalExceptionHandler.java
@@ -28,50 +28,56 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public RestResponse<String> handleException(Exception ex, HttpServletRequest request) {
+    public ResponseEntity<RestResponse<Void>> handleException(Exception ex, HttpServletRequest request) {
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         String uri = request.getRequestURI();
 
-        return RestResponse.error(
-            status.value(),
-            ex.getMessage(),
-            "000", // Código de error para regla de negocio
-            uri,
-            status.getReasonPhrase() // Detailed error message like "Internal Server Error" or "Not Found"
-        ); 
+        return ResponseEntity
+                .status(status)
+                .body(RestResponse.error(
+                    status.value(),
+                    ex.getMessage(),
+                    "000", // Código de error para regla de negocio
+                    uri,
+                    status.getReasonPhrase() // Detailed error message like "Internal Server Error" or "Not Found"
+                ));
 
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public RestResponse<String> handleEntityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<RestResponse<Void>> handleEntityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
 
         HttpStatus status = HttpStatus.NOT_FOUND;
         String uri = request.getRequestURI();
 
-        return RestResponse.error(
-            status.value(),
-            ex.getMessage(),
-            ex.getCode(), // Código de error para regla de negocio
-            uri,
-            status.getReasonPhrase() // Detailed error message like "Not Found"
-        ); 
+        return ResponseEntity
+                .status(status)
+                .body(RestResponse.error(
+                    status.value(),
+                    ex.getMessage(),
+                    ex.getCode(), // Código de error para regla de negocio
+                    uri,
+                    status.getReasonPhrase() // Detailed error message like "Not Found"
+                ));
 
     }
 
     @ExceptionHandler(BusinessRuleException.class)
-    public RestResponse<String> handleBusinessRuleException(BusinessRuleException ex, HttpServletRequest request) {
+    public ResponseEntity<RestResponse<Void>> handleBusinessRuleException(BusinessRuleException ex, HttpServletRequest request) {
 
         HttpStatus status = HttpStatus.BAD_REQUEST;
         String uri = request.getRequestURI();
 
-        return RestResponse.error(
-            status.value(),
-            ex.getMessage(),
-            ex.getCode(), // Código de error para regla de negocio
-            uri,
-            status.getReasonPhrase() // Detailed error message like "Bad Request"
-        ); 
+        return ResponseEntity
+                .status(status)
+                .body(RestResponse.error(
+                    status.value(),
+                    ex.getMessage(),
+                    ex.getCode(), // Código de error para regla de negocio
+                    uri,
+                    status.getReasonPhrase() // Detailed error message like "Bad Request"
+                ));
 
     }
 
