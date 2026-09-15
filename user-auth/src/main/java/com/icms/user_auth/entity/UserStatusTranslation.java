9e7ed3bdda517ac0a4da80cbb82d927408b91050
Maximo Barroeta <ketonco@gmail.com>
2026-09-15 03:44:11 +0200

    feat(UserStatusTranslationService) - Enhance UserStatusTranslationService with new methods for fetching translations by catalog and language

--- user-auth/src/main/java/com/icms/user_auth/entity/UserStatusTranslation.java
+++ user-auth/src/main/java/com/icms/user_auth/entity/UserStatusTranslation.java
@@ -5,9 +5,11 @@ import com.icms.shared.entity.Language;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "userstatus_translation")
+@NoArgsConstructor 
 public class UserStatusTranslation extends BaseCatalogTranslationEntity<UserStatus>{
 
     public UserStatusTranslation(UserStatus catalog, Language language, String translation, String description) {
--- user-auth/src/main/java/com/icms/user_auth/service/daoservice/UserStatusTranslationService.java
+++ user-auth/src/main/java/com/icms/user_auth/service/daoservice/UserStatusTranslationService.java
@@ -1,7 +1,14 @@
 package com.icms.user_auth.service.daoservice;
 import com.icms.user_auth.dto.userstatus.UserStatusTranslationDto;
 import com.icms.user_auth.entity.UserStatus;
+import com.icms.user_auth.repository.UserStatusRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.icms.shared.entity.Language;
+import com.icms.shared.exceptions.EntityNotFoundException;
 import com.icms.user_auth.dto.userstatus.UserStatusTranslationMapper;
 
 import com.icms.shared.service.BaseCatalogTranslationService;
@@ -14,9 +21,27 @@ public class UserStatusTranslationService extends BaseCatalogTranslationService<
                                                                                 UserStatusTranslationDto, 
                                                                                 UserStatusTranslationRepository,
                                                                                 UserStatus>{
-    
+    UserStatusTranslationRepository repository;
+    UserStatusTranslationMapper mapper; 
+    @Autowired
+    UserStatusRepository userStatusRepository; 
+
     public UserStatusTranslationService(UserStatusTranslationRepository repository, UserStatusTranslationMapper mapper, UserStatusTranslationRules rules) {
         super(repository, mapper, rules);
+        this.repository = repository;
+        this.mapper = mapper;
     }
 
+    public UserStatusTranslation findByCatalogAndLanguage(UserStatus catalog, Language language) {
+        return repository.findByCatalogAndLanguage(catalog, language).orElse(null);
+    }
+
+    public UserStatusTranslationDto findByCatalogAndLanguageDto(UserStatus catalog, Language language) {
+        return mapper.toDto(findByCatalogAndLanguage(catalog, language));
+    }
+
+    public List<UserStatusTranslationDto> findByCatalogId(Long catalogId) {
+        UserStatus catalog = userStatusRepository.findById(catalogId).orElseThrow(() -> new EntityNotFoundException());
+        return findByCatalog(catalog).stream().map(mapper::toDto).toList();
+    }
 }
