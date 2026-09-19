adc604a1d22b5488855d191ed05b27999f2fafd7
Maximo Barroeta <ketonco@gmail.com>
2026-09-19 12:36:58 +0200

    fix(MapperSetting) - Add null value property mapping strategy to MapperConfig

--- shared-kernel/src/main/java/com/icms/shared/config/mapper/MapperSetting.java
+++ shared-kernel/src/main/java/com/icms/shared/config/mapper/MapperSetting.java
@@ -2,11 +2,13 @@ package com.icms.shared.config.mapper;
 
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @MapperConfig(
     componentModel = "spring",
-    unmappedTargetPolicy = ReportingPolicy.IGNORE
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
 )
 public interface MapperSetting {
-
+ //TODO: create a test to validate the mapper settings to ignore unmapped targets and null value property mapping
 }
