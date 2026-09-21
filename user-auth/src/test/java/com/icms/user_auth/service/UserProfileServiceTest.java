0c17f7697ce220b9923c6e7de1bb3b226b6ad346
Maximo Barroeta <ketonco@gmail.com>
2026-09-21 16:23:49 +0200

    test(UserProfileService) - Add initial test class for UserProfileService with Mockito support

---
+++ user-auth/src/test/java/com/icms/user_auth/service/UserProfileServiceTest.java
@@ -0,0 +1,11 @@
+package com.icms.user_auth.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Profile;
+
+@ExtendWith(MockitoExtension.class)
+@Profile("test")
+public class UserProfileServiceTest {
+
+}
